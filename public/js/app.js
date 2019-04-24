9c3d7c18b29b255139568f2f9dd805b1fdbe291e
Sarah Maples <sarah.e.maples@gmail.com>
2019-04-23 21:10:17 -0500

    mongodb basic setup started. models folder coming along nicely

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+
+// // Create an object containing dummy data to save to the database
+// var data = {
+//     title: "harry potter and the half blood prince",
+//     link: "tee@hee.com"
+// };
+
+// // Save a new Example using the data object
+// News.create(data)
+//     .then(function(dbExample) {
+//         // If saved successfully, print the new Example document to the console
+//         console.log(dbExample);
+//     })
+//     .catch(function(err) {
+//         // If an error occurs, log the error message
+//         console.log(err.message);
+// });
+
+// $.get("/test").then(function(data) {
+//     console.log(data);
+// });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var logger = require("morgan");
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 // Our newest addition to the dependency family
@@ -6,18 +7,20 @@ var mongoose = require("mongoose");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
-// // It works on the client and on the server
-// var axios = require("axios");
-// var cheerio = require("cheerio");
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-// // Require all models
-// var db = require("./models");
+// Require all models
+var db = require("./models");
 
 var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,6 +33,23 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/scrapeyScrapes", { useNewUrlParser: true });
+
+// Create an object containing dummy data to save to the database
+var data = {
+    title: "harry potter and the sorcerers stone",
+    link: "tee@hee.com"
+};
+
+// Save a new Example using the data object
+db.News.create(data)
+    .then(function(moreData) {
+        // If saved successfully, print the new Example document to the console
+        console.log(moreData);
+    })
+    .catch(function(err) {
+        // If an error occurs, log the error message
+        console.log(err.message);
+});
 
 // here you need to use you api routes and stuff
 var controller = require("./controllers/html-routes");
