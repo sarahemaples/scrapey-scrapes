a7aa8c2ed473b8d3ac1d0a44e1cabdfc0f1ef324
Sarah Maples <sarah.e.maples@gmail.com>
2019-04-24 20:19:44 -0500

    minor styling added. time to work on commentssss

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
 // Grab the articles as a json
 $.getJSON("/pastas", function(data) {
     // For each one
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 5; i < data.length; i++) {
+      var html = "";
+      // render cards for each pasta
+      html += "<div class='card'><div class='card-header'><h3>";
+      html += data[i].title;
+      html += "</div><div class='card-body'><h5>";
+      html += "<p data-id='" + data[i]._id + "'>";
+      html += data[i].teaser;
+      html += "</p></h5></div><div class='card-footer'>";
+      html += "<a href='" + data[i].link + "' target='blank'>full story</a>";
+      html += "</div>";
       // Display the apropos information on the page
-      $("#pastas").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].teaser + "<br />" + data[i].link + "</p>");
+      // $("#pastas").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].teaser + "<br />" + data[i].link + "</p>");
+      $("#pastas").append(html);
     }
   });
 
@@ -13,3 +24,5 @@ $(document).on("click", "button", function() {
         console.log(data);
     })
 })
+
+// 11
